6f815bd4ebaf656684ba3b9e98279bdde0ff97aa
GSERBAN <gabriel.serban@globallogic.com>
2024-09-13 09:51:49 +0300

    Added swagger

--- Program.cs
+++ Program.cs
@@ -4,10 +4,14 @@ using Service;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<GroupExceptionMiddleware>();
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<GroupService>();
 builder.Services.AddSingleton<DbContext>();
 
 var app = builder.Build();
 app.UseMiddleware<GroupExceptionMiddleware>();
 app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 app.Run();
